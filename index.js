0a3e938528b0ac90a1a3df7251ac9f828a667f66
kauapripra <kauapriprapenz@gmail.com>
2023-12-13 10:11:38 -0300

    teste

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const pessoa = require("./pessoa")
 
 const app = express()
 app.use(express.json())
-
+//comentario teste
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
